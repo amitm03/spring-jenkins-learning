60a42b5ad1c14c58aac1a9974dc584078f596749
Amit_Mishra03 <amit.mishra369@gmail.com>
2023-12-03 20:06:47 +0530

    Initial commit

---
+++ src/main/java/com/jenkins/springbootjenkinsapplication/SpringbootjenkinsapplicationApplication.java
@@ -0,0 +1,23 @@
+package com.jenkins.springbootjenkinsapplication;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjenkinsapplicationApplication {
+	public static Logger logger = LoggerFactory.getLogger(SpringbootjenkinsapplicationApplication.class);
+
+	@PostConstruct
+	public void intt(){
+		logger.info("Application started...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(SpringbootjenkinsapplicationApplication.class, args);
+	}
+
+}
